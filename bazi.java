b42e2e7b5442e8ef91dcbe08ae7508d65b7d8c40
mahshad <mahshadch1381@gmail.com>
2021-03-26 15:02:30 +0430

    roles added

--- bazi.java
+++ bazi.java
@@ -1,9 +1,30 @@
 import java.util.Scanner;
 public class bazi {
     static bazikon []players =new bazikon[1000];
+     static int n;
     public static void create(String [] a ,int n){
        for (int i=0;i<n;i++){
          players[i].name=a[i];
+        }
+    }
+    public static void assign(String a){
+        int s1=a.indexOf(" ");
+        a=a.substring(s1+1);
+        int s2=a.indexOf(" ");
+        String b=a.substring(0,s2);
+        String c=a.substring(s2+1);
+        int x=0;
+        for (int i=0;i<n;i++){
+            if(players[i].name.equals(b)){
+                players[i].teeen_naghsh(c);
+                break;
+            }
+            else {
+                x++;
+            }
+        }
+        if(x==n){
+            System.out.println("user not found");
         }
     }
     public static void main(String args){
@@ -15,7 +36,6 @@ public class bazi {
                 int space=a.indexOf(" ");
                  String c1=a.substring(space+1);
                 String []c2=new String[1000];
-                int n=0;
                  for (int j=0;c1.indexOf(" ")!=0;j++){
                       space=c1.indexOf(" ");
                       String p=c1.substring(0,space);
@@ -32,6 +52,10 @@ public class bazi {
                 continue;
             }
             else {
+                if(a.contains("assign")){
+                  assign(a);
+                }
+
 
             }
 
--- bazikon.java
+++ bazikon.java
@@ -1,11 +1,47 @@
 public class bazikon {
-    String name;
-    String naghsh1;
-    int naghsh;
-    int nightvote;
-    int dayvote;
-    boolean nejat_pezeshk;
-    boolean silent;
-
+   static String name;
+  static String naghsh1;
+   static int naghsh;
+   static int nightvote;
+    static int dayvote;
+   static boolean nejat_pezeshk;
+   static boolean silent;
+    public static void teeen_naghsh(String n){
+        if(n.equals("mafia")){
+            naghsh1="mafia";
+            naghsh=1;
+        }
+       else if(n.equals("doctor")){
+            naghsh1="doctor";
+            naghsh=-3;
+        }
+       else if(n.equals("detective")){
+            naghsh1="detective";
+            naghsh=-2;
+        }
+       else if(n.equals("villager")){
+            naghsh1="villager";
+            naghsh=-1;
+        }
+       else if(n.equals("Joker")){
+            naghsh1="Joker";
+            naghsh=0;
+        }
+       else if(n.equals("bulletproof")){
+            naghsh1="bulletproof";
+            naghsh=-4;
+        }
+      else if(n.equals("godfather")){
+            naghsh1="godfather";
+            naghsh=3;
+        }
+      else   if(n.equals("silencer")){
+            naghsh1="silencer";
+            naghsh=2;
+        }
+      else {
+          System.out.println("role not found");
+        }
+    }
 
 }
